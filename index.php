ceb4fe99cf8bf97d3fd0b196fc000c21d9018725
Cloud <ruben.as532@gmail.com>
2024-02-29 10:48:58 +0100

    Factorial is complete

--- index.php
+++ index.php
@@ -8,5 +8,16 @@ function countDown(int $n): void {
     }
 }
 
-countDown(3);
+function getFactorial(int $n): mixed {
+    if($n == 0 || $n == 1) return 1;
+    else return $n*getFactorial($n-1);
+}
+
+$n = readline("Cuenta atrás desde...");
+countDown(intval($n));
+echo PHP_EOL;
+//TO-DO: Interesante que tenga que poner el tipo a mixed.
+$n = readline("Dime de qué número quieres calcular el factorial?");
+echo "El factorial de {$n} es ".getFactorial(intval($n));
+
 ?>
