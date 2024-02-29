64de22ca5861c13af481a2af96b28da6df9dbac7
Cloud <ruben.as532@gmail.com>
2024-02-29 11:02:46 +0100

    Okay, what I did in 27 minutes. Need coffee or something more to solve today that fibonazzi recursion :)

--- index.php
+++ index.php
@@ -13,11 +13,30 @@ function getFactorial(int $n): mixed {
     else return $n*getFactorial($n-1);
 }
 
+function printFibonacciSerie($n): ?int {
+    if($n == 0) {
+        echo 0;
+        return 0;
+    }
+    else if($n == 1) {
+        echo 1;
+        return 1;
+    }
+    else {
+        $n = printFibonacciSerie($n-1) + printFibonacciSerie($n-2);
+        echo $n;
+        return $n;
+    }
+} 
+/*
 $n = readline("Cuenta atrás desde...");
 countDown(intval($n));
 echo PHP_EOL;
 //TO-DO: Interesante que tenga que poner el tipo a mixed.
 $n = readline("Dime de qué número quieres calcular el factorial?");
-echo "El factorial de {$n} es ".getFactorial(intval($n));
-
+echo "El factorial de {$n} es ".getFactorial(intval($n)).PHP_EOL;
+*/
+$n = readline("Serie de Fibonacci hasta...");
+printFibonacciSerie(intval($n));
+echo PHP_EOL;
 ?>
